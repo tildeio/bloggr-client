bfe6ef0ccf2d1e179748eb5c90a8ec1971d03fb2
Brynhuda <brynhuda@tilde.io>
2013-03-23 16:52:19 -0700

    Get started with static HTML

--- js/app.js
+++ js/app.js
@@ -1,7 +1,1 @@
 App = Ember.Application.create({});
-
-App.IndexRoute = Ember.Route.extend({
-  setupController: function(controller) {
-    controller.set('content', ['red', 'yellow', 'blue']);
-  }
-});
