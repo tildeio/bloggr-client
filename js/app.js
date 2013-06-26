55cc9349517cabdd486348b052c23bd31e1e9862
Kevin Lamping <klamping@gmail.com>
2013-06-26 14:21:57 -0500

    Added default route from 'index' to 'posts' to match screencast

--- js/app.js
+++ js/app.js
@@ -14,6 +14,12 @@ App.Router.map(function() {
   });
 });
 
+App.IndexRoute = Ember.Route.extend({
+  redirect: function () {
+    this.transitionTo('posts');
+  }
+});
+
 App.PostsRoute = Ember.Route.extend({
   model: function() {
     return App.Post.find();
