a7ded52ca649d7a2352a7e0db6ed8c0f418ad5ae
Ahmad Alhashemi <ahmad@ahmadh.com>
2015-05-14 23:19:46 -0400

    Upgraded to: - Ember 1.12.0 - Ember Template Compiler 1.12.0 - normalize.css 3.0.2 - jQuery 1.11.3 - Showdown 0.5.0 - Moment.js 2.10.2

--- js/app.js
+++ js/app.js
@@ -52,7 +52,7 @@ App.PostController = Ember.ObjectController.extend({
 var showdown = new Showdown.converter();
 
 Ember.Handlebars.helper('format-markdown', function(input) {
-  return new Handlebars.SafeString(showdown.makeHtml(input));
+  return new Ember.Handlebars.SafeString(showdown.makeHtml(input));
 });
 
 Ember.Handlebars.helper('format-date', function(date) {
